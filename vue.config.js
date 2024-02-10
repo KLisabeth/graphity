0a85e353f1ec4d1c86cc7c4ae6b7809226620a73
KLisabeth <katie_lisabeth@yahoo.com>
2024-02-10 22:50:33 +0100

    Build for deployment

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+import { defineConfig } from '@vue/cli-service'
+export default defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/graphity/'
+    : '/',
   transpileDependencies: true
 })
